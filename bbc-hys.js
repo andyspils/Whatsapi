1f76747d1fbea5338a42b75925543241e2964ae8
Andy <andyspils@hotmail.com>
2025-07-31 11:03:21 +0100

    changes 31/7

--- bbc-hys.js
+++ bbc-hys.js
@@ -1,11 +1,11 @@
 const fs = require('fs');
 const { text } = require('stream/consumers');
-const getPage = require('./bbc-hys-page.js');
 
 
 let ct = 0; let ix = 0; let fn = 0;
 let urls = []; let finish = []; comments = [];
-let pages = ["sport", "sport/football", "sport/cricket", "sport/formula1", "sport/rugby-union", "sport/tennis", "sport/athletics", "sport/golf", "sport/boxing", "news", "news/world", "news/business", "weather", "news/health", "news/science_and_environment", "news/technology", "news/entertainment_and_arts", "news/education", "news/uk_politics", "news/in_pictures", "news/video"];
+// let pages = ["sport", "sport/football", "sport/cricket", "sport/formula1", "sport/rugby-union", "sport/tennis", "sport/athletics", "sport/golf", "sport/boxing", "news", "news/world", "news/business", "weather", "news/health", "news/science_and_environment", "news/technology", "news/entertainment_and_arts", "news/education", "news/uk_politics", "news/in_pictures", "news/video"];
+let pages = ["sport", "sport/football"];
 
 // Function to fetch BBC HYS (Have Your Say) URLs from a given subPage
 async function getBbcHys(subPage, fn) {
@@ -60,29 +60,66 @@ async function getBbcHys(subPage, fn) {
             regex = /forumId=([^&]+)/;
             var forumId = urlHtm.match(regex);
 
+            let fullUrl = "https://www.bbc.co.uk/wc-data/container/comments?apiKey=" + apiKey[1] + "&env=live&forumId=" + forumId[1] + "&isFirstDataRequested=true";
             // fetch from comments url to get nextToken
-            const getNextToken = await fetch("https://www.bbc.co.uk/wc-data/container/comments?apiKey=" + apiKey[1] + "&env=live&forumId=" + forumId[1] + "&isFirstDataRequested=true", {
+            const getNextToken = await fetch(fullUrl, {
                 "body": null,
                 "method": "GET"
             });
 
             const nextTokenJson = await getNextToken.json();
 
-            urls[i] = { url: urls[i], title: nextTokenJson.title, moderation: nextTokenJson.moderation.status, totalPostsCount: nextTokenJson.totalPostsCount, totalCommentsCount: nextTokenJson.totalCommentsCount, canLoadMore: nextTokenJson.canLoadMore, isClosed: nextTokenJson.isClosed, apiKey: apiKey[1], forumId: forumId[1], nextToken: nextTokenJson.nextToken, comments: nextTokenJson.comments  };
-           
+            urls[i] = { url: urls[i], fullUrl: fullUrl, title: nextTokenJson.title, moderation: nextTokenJson.moderation.status, totalPostsCount: nextTokenJson.totalPostsCount, totalCommentsCount: nextTokenJson.totalCommentsCount, canLoadMore: nextTokenJson.canLoadMore, isClosed: nextTokenJson.isClosed, apiKey: apiKey[1], forumId: forumId[1], nextToken: encodeURI(nextTokenJson.nextToken), comments: nextTokenJson.comments };
+
         }
         // clear down json file 
         fs.writeFileSync("bbc-hys.json", JSON.stringify([], null, 3), 'utf8');
         // Write the URLs to a JSON file
         fs.writeFileSync("bbc-hys.json", JSON.stringify(urls, null, 3), 'utf8');
         console.log("Ending BBC HYS URL extraction at " + new Date().toLocaleString());
-      
+
+        // test
+        getAllPages("/sport/cricket/articles/c1wpyq7qr2zo", true, "");
+            
+        
+
+    }
+    return true
+}
+
+// getAllPages for a comment section
+async function getAllPages(page, first, data) {
+//console.log(page);
+    if (first === true) {
+        data = JSON.parse(fs.readFileSync('bbc-hys.json', 'utf8'));
+        result = data.find(data => data.url === page);
+        apiKey = result.apiKey;
+        forumId = result.forumId;   
+        nextToken = result.nextToken;
+    } else {
+        nextUrl = "https://www.bbc.co.uk/wc-data/container/comments?apiKey=" + apiKey + "&env=live&forumId=" +forumId + "&isFirstDataRequested=true&isLeftAligned=false" + "&nextToken=" + encodeURIComponent(nextToken) + "&palette=standard&showViewCommentsButton=true&siteId=newscommentsmodule&siteTitle=BBC&sortOrder=HighestRated&urnForUAS=urn%3Abbc%3Aoptimo%3Aasset%3Ackgy7jx082ro&useMaxReadableWidth=true&webcoreCommentsEnabled=on&wrapWidth=full";
+        // fetch from comments url to get nextToken
+        console.log(nextUrl);
+        data = await fetch(nextUrl, {
+            "body": null,
+            "method": "GET"
+        });
+       
+        result = await data.json(); console.log(data);        
+        //result = data.find(data => data.url === page);
+        
+    }
+
+    if (result.canLoadMore === true) {
+        console.log("load next:", encodeURIComponent(result.nextToken));
+        getAllPages(page + "&nextToken=" + encodeURIComponent(result.nextToken), false, result, apiKey, forumId);
+    } else {
+        console.log("No match found.");
+    }
+}
 
 
-  
-}
-return true
-}
+
 
 // Start the process, processing each page
 
